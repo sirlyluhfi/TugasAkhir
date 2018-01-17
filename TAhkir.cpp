7f58b9e4032a46660496d0fc11375658398c5ce7
sirlyluthfi <32449911+sirlyluhfi@users.noreply.github.com>
2018-01-17 15:25:35 +0700

    Memasukkan if else kedalam class

--- TAhkir.cpp
+++ TAhkir.cpp
@@ -41,3 +41,43 @@ void Tas ::Bahan(string material){
     TasSekolah.harga = "100.000-500.000";
     TasSekolah.JumlahTas = 100;
     TasSekolah.merek = "eiger";
+    
+string pilihan;
+    cout << "pilih Tas : "; cin >> pilihan;
+    //
+    if (pilihan== "TasGunung"){
+    	 cout << "Tas Gunung : "<<endl;
+    	 cout << "Harga tas : "<<(TasGunung.harga)<<endl;
+         cout << "Ukuran Tas = "<<(TasGunung.JumlahTas)<<" pics "<<endl;
+         cout << "Merek Tas =  "<<(TasGunung.merek)<<endl;
+    TasGunung.Fungsi("Membawa Perlengkapan Hiking");
+    TasGunung.Ketahanan("Mampu membawa beban  max 50kg");
+    TasGunung.Bahan("Polyester Nyilon ");
+    //
+	}else if(pilihan=="TasKerja"){
+		 cout << "Tas Kerja : "<<endl;
+		 cout << "Harga tas : "<<(TasKerja.harga)<<endl;
+   		 cout << "Ukuran Tas = "<<(TasKerja.JumlahTas)<<" pics "<<endl;
+    	 cout << "Merek Tas =  "<<(TasKerja.merek)<<endl;
+    TasKerja.Fungsi("Membawa Perlengkapan kerja,Laptop DLL");
+    TasKerja.Ketahanan("Mampu memuat Beban  max 10kg");
+	TasKerja.Bahan("kulit,denim,kanvas");
+	//
+	}else if(pilihan=="TasSekolah"){
+		cout << "Tas Sekolah: "<<endl;
+		cout << "Harga tas : "<<(TasSekolah.harga)<<endl;
+        cout << "Ukuran Tas = "<<(TasSekolah.JumlahTas)<<" pisc "<<endl;
+        cout << "Merek Tas =  "<<(TasSekolah.merek)<<endl;
+        
+    TasSekolah.Fungsi("Membawa Perlengkapan Sekolah,Laptop,Alat Tulis DLL");
+    TasSekolah.Ketahanan("Mampu memuat Beban max 5kg");
+    TasSekolah.Bahan("Polyester,nyilon");
+	}else
+	{
+		cout<< "Nama Yang Anda Masukan Kurang Tepat";
+	}
+	
+   
+    
+ 	
+ }
